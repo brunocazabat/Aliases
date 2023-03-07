7dd16c4b38d54235ee0ff98e667610156ecd9717
Bruno Cazabat <bruno.cazabat@gmail.com>
2023-03-07 19:50:41 +0100

    Update init.bat

--- init.bat
+++ init.bat
@@ -1,4 +1,4 @@
 @echo off
-Invoke-WebRequest "https://raw.githubusercontent.com/brunocazabat/repository-tmpl/main/myconfig.sh" -OutFile ./script.sh
+powershell -Command "Invoke-Webrequest https://raw.githubusercontent.com/brunocazabat/repository-tmpl/main/myconfig.sh -OutFile ./script.sh" & cls
 bash ./script.sh
 rm ./script.sh
