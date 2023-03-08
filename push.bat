359909ad8ecf539749b960856ed5219c01eb5705
Bruno Cazabat <bruno.cazabat@gmail.com>
2023-03-08 15:58:42 +0100

    0.0.1

--- push.bat
+++ push.bat
@@ -1,5 +1,7 @@
 @echo off
+@echo "Pushing changes to main branch"
 git add .
 set /p tag=Enter incoming commit name:
 git commit -m "%tag%"
-git push
+git pull origin main
+git push origin main
--- tag.bat
+++ tag.bat
@@ -1,5 +1,8 @@
 @echo off
+@echo "Creating new tag and pushing it to main branch"
 git tag --sort=-v:refname
-set /p tag=Enter incoming tag name:
+@push
+set /p tag=Please enter the incoming tag name:
 git tag "%tag%"
+git pull origin main
 git push origin "%tag%"
