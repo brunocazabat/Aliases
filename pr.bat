8b5db920a02e9d6227d318ec77901e6a171d7be7
Bruno Cazabat <bruno.cazabat@gmail.com>
2023-03-08 17:35:44 +0100

    Feat: Pull Request is automated

---
+++ pr.bat
@@ -0,0 +1,12 @@
+@echo off
+set datetimef=%date:~-4%_%date:~3,2%_%date:~0,2%_%time:~0,2%_%time:~3,2%_%time:~6,2%
+cls
+@echo "Pushing changes to a dedicated branch and asking for a pull request"
+git checkout -b PullRequestAsked-%datetimef%
+git push -u origin PullRequestAsked-%datetimef%
+echo "ta mere la pute"
+git add .
+git commit -m "Pull request asked at %datetimef%"
+git push
+git request-pull main ./
+git checkout main
